986b4f58b0e67f3b27e4630d45f8a85300c4309e
a289237642 <a289237642@163.com>
2020-07-25 22:38:15 +0800

    week7

---
+++ Week07/208. 实现 Trie (前缀树).py
@@ -0,0 +1,82 @@
+"""
+实现一个 Trie (前缀树)，包含 insert, search, 和 startsWith 这三个操作。
+
+示例:
+
+Trie trie = new Trie();
+
+trie.insert("apple");
+trie.search("apple");   // 返回 true
+trie.search("app");     // 返回 false
+trie.startsWith("app"); // 返回 true
+trie.insert("app");   
+trie.search("app");     // 返回 true
+说明:
+
+你可以假设所有的输入都是由小写字母 a-z 构成的。
+保证所有输入均为非空字符串
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/implement-trie-prefix-tree
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+class TrieNode:
+    def __init__(self):
+        self.children = defaultdict(TrieNode)
+        self.is_word = False
+
+
+class Trie:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = TrieNode()
+
+
+    def insert(self, word: str) -> None:
+        """
+        Inserts a word into the trie.
+        """
+        node = self.root
+        for s in word:
+            node = node.children[s]
+        node.is_word = True
+
+
+    def search(self, word: str) -> bool:
+        """
+        Returns if the word is in the trie.
+        """
+        node = self.root
+        for s in word:
+            if s in node.children:
+                node = node.children[s]
+            else:
+                return False
+        return node.is_word
+
+
+    def startsWith(self, prefix: str) -> bool:
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        """
+        node = self.root
+        for s in prefix:
+            if s in node.children:
+                node = node.children[s]
+            else:
+                return False
+        return True
+        
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
+
+
---
+++ Week07/212. 单词搜索 II.py
@@ -0,0 +1,56 @@
+
+"""
+给定一个二维网格 board 和一个字典中的单词列表 words，找出所有同时在二维网格和字典中出现的单词。
+
+单词必须按照字母顺序，通过相邻的单元格内的字母构成，其中“相邻”单元格是那些水平相邻或垂直相邻的单元格。同一个单元格内的字母在一个单词中不允许被重复使用。
+
+示例:
+
+输入: 
+words = ["oath","pea","eat","rain"] and board =
+[
+  ['o','a','a','n'],
+  ['e','t','a','e'],
+  ['i','h','k','r'],
+  ['i','f','l','v']
+]
+
+输出: ["eat","oath"]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/word-search-ii
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        trie = {}
+        for word in words:
+            t = trie
+            for w in word:
+                t = t.setdefault(w, {})
+            t["end"] = 1
+        #print(trie)
+        res = []
+        row = len(board)
+        col = len(board[0])
+        def dfs(i, j, trie, s):
+            #print(i, j, trie, s)
+            c = board[i][j]
+            if c not in trie: return
+            trie = trie[c]
+            if "end" in trie and trie["end"] == 1:
+                res.append(s + c)
+                trie["end"] = 0 # 防止重复数组加入
+            board[i][j] = "#"
+            for x, y in [[-1, 0], [1, 0], [0, 1], [0, -1]]:
+                tmp_i = x + i
+                tmp_j = y + j
+                if 0 <= tmp_i < row and 0 <= tmp_j < col and board[tmp_i][tmp_j] != "#":
+                    dfs(tmp_i, tmp_j, trie, s + c)
+            board[i][j] = c
+        for i in range(row):
+            for j in range(col):
+                dfs(i, j, trie, "")
+        return res
+
